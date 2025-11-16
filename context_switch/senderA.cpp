1e9d24917c1a29bc6f6d373ac94668f182179737
Abukar-1000 <abukardjama1@gmail.com>
2025-11-16 04:26:52 -0500

    [Context Switch] init

---
+++ context_switch/senderA.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<chrono>
+
+
+int main() {
+    int count = 0;
+    const int LIMIT = 2147483647;
+    
+    while (count <= LIMIT) {
+
+    }
+    return 0;
+}
---
+++ context_switch/senderB.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+#include<chrono>
+
+
+int main() {
+
+    return 0;
+}
